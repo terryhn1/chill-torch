017e21f50ea8ee87c01c894337440d77ad842ebe
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2023-04-18 12:42:19 -0700

    setting up structure for visualizer

--- chill_visualizer/chill_reccs.py
+++ chill_visualizer/chill_reccs.py
@@ -1,3 +1,33 @@
 
 class ChillRecommenderEngine:
-    pass
+    def __init__(self, dataset, dataset_type, decision_factor = 'corr'):
+        
+        # Activate certain functionalities depending on the dataset_type
+        if dataset_type == 'numerical':
+            self._activate_numerical_analysis()
+        elif dataset_type == 'image':
+            self._activate_image_analysis()
+        elif dataset_type == 'audio':
+            self._activate_audio_analysis()
+        elif dataset_type == 'video':
+            self._activate_video_analysis()
+        elif dataset_type == 'text':
+            self._activate_text_analysis()
+    
+    def _activate_numerical_analysis(self):
+        pass
+
+    def _activate_image_analysis(self):
+        pass
+
+    def _activate_audio_analysis(self):
+        pass
+
+    def _activate_video_analysis(self):
+        pass
+
+    def _activate_text_analysis(self):
+        pass
+
+    def get_top_k_graph_choices(self):
+        pass
--- chill_visualizer/chill_visualizer.py
+++ chill_visualizer/chill_visualizer.py
@@ -6,11 +6,12 @@ import torch
 from torch.utils.data import Dataset
 
 class ChillVisualizer:
-    def __init__(self, model: ChillModel, dataset: Dataset, dataset_type: str):
+    def __init__(self, model: ChillModel, dataset: Dataset, dataset_type: str, grid_theme_color: str = "whitegrid"):
         
         self.model = model
         self.dataset = dataset
-        self.dataset_type = dataset_type    
+        self.dataset_type = dataset_type
+        self.grid_theme_color=  grid_theme_color
 
     def __call__(self, **kwargs):
         """ Allows the user to change the attributes if desired.
@@ -20,25 +21,54 @@ class ChillVisualizer:
                 raise AttributeError(f"Attribute {key} cannot be assigned")
             self.__dict__[key] = value
 
-    def visualize(self):
+    def visualize(self, graph_requests = [], k = 1):
+        recc_engine = ChillVisualRecommender(self.dataset, self.dataset_type)
+
+        if not graph_requests:
+            reccs = recc_engine.get_top_k_graph_choices()
+
+            for recc in reccs:
+                if recc == 'displot':
+                    self._dis_plot()
+                elif recc == 'hist':
+                    self._hist()
+                elif recc == "line_plot":
+                    self._line_plot()
+                elif recc == "heatmap":
+                    self._heatmap()
+                elif recc == 'images':
+                    self._plot_k_images()
+        else:
+            pass
+            # Do what the user wants
+
+
+    def plot_preds(self):
+        # This is a manual plot, rather than using tensorboard
+        train_loss, train_acc = self.model.train_results
+        test_loss, test_acc = self.model.test_results
+
+        sns.set_theme("whitegrid")
         pass
 
-    def _count_plot(self):
+    def _dis_plot(self):
+        sns.displot()
         pass
 
     def _hist(self):
+        sns.histplot()
         pass
 
     def _line_plot(self):
+        sns.lineplot(...)
         pass
 
     def _heatmap(self):
+        sns.heatmap(...)
         pass
 
-    def _accuracy_to_loss_plot(self):
-        pass
-
-    def plot_k_images(self, k = 1, random_seed = None):
+    def _plot_k_images(self, k = 1, random_seed = None):
+        # use matplotlib plot charts with PIL images here
         pass
 
         
